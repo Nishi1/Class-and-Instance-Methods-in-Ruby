3c957cffc1e5f7953b72272f0e2c014c7179bfc2
Nishi Kant Sharma <nishiit24@gmail.com>
2019-05-07 15:42:02 +0530

    Adding and accessing nested classes in ruby

---
+++ access_inner_classes.rb
@@ -0,0 +1,36 @@
+=begin
+Nested classes in Ruby Implementation
+=end
+class Drawing
+  def self.what_am_i #  Drawing.what_am_i
+    "This is a main class method"
+  end
+
+  class Triangle
+	def what_am_i
+      "This is a triangle class methos"
+    end
+  end
+
+  class Circle
+    def what_am_i
+      "This is a circle class methos"
+    end
+  end
+end
+
+a = Drawing.what_am_i
+puts a
+
+a = Drawing::Triangle.new
+puts a.what_am_i
+
+a = Drawing::Circle.new
+puts a.what_am_i
+
+=begin
+Output
+This is a main class method
+This is a triangle class methos
+This is a circle class methos
+=end
